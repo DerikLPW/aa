857a98748d734315e0190484346477ec79c117d6
Derik.Wu <derik.wu@waclighting.com.cn>
2021-12-21 10:14:00 +0800

    1.Add Container Activity.

--- app/src/main/java/com/example/phone/ContainerActivity.kt
+++ app/src/main/java/com/example/phone/ContainerActivity.kt
@@ -1,16 +1,19 @@
 package com.example.phone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.phone.fragment.DrawableAnimFragment
 import com.example.phone.fragment.LineChartFragment
 import com.example.phone.fragment.ScrollSlideFragment
 
 class ContainerActivity : AppCompatActivity() {
+    private val TAG = "ContainerActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e(TAG, "onCreate")
         setContentView(R.layout.activity_container)
 
 //        startScrollSlide()
@@ -39,4 +42,34 @@ class ContainerActivity : AppCompatActivity() {
         replaceFragment(LineChartFragment.newInstance(), "LineChartFragment", false)
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(TAG, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy")
+    }
 }
--- app/src/main/java/com/example/phone/MainActivity.kt
+++ app/src/main/java/com/example/phone/MainActivity.kt
@@ -1,28 +1,52 @@
 package com.example.phone
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e(TAG, "onCreate")
         setContentView(R.layout.activity_main)
 
-        lcv.bgColor = Color.GRAY
-        lcv.items = floatArrayOf(
-            60f,
-            100f,
-            0f,
-            23f,
-            170f,
-            50f,
-            10f
-        )
-        lcv.setOnClickListener{
-            println("lcv clicked")
+        btn_start.setOnClickListener {
+            startActivity(Intent(this, ContainerActivity::class.java))
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(TAG, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy")
     }
 }
